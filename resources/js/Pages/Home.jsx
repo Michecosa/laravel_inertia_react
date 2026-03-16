73b2d5efb3103516ee321af0ab18781a019b0174
Michela <micheladellagatta1@gmail.com>
2026-03-16 12:54:03 +0100

    feat: setup Inertia.js client-side +TailwindCSS

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="title">Hello!</h1>
+        </div>
+    );
+}
